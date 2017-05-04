e156bbede57cd3b5f3090136daf034f02459b430
Michael Fogel <mike@fogel.ca>
2017-05-04 19:23:52 -0300

    Getting the navbar decent

--- src/App.js
+++ src/App.js
@@ -1,47 +1,33 @@
 import React, { Component } from 'react';
+import { Navbar, FormGroup, FormControl, Button, ButtonGroup, ButtonToolbar } from 'react-bootstrap';
 
 class App extends Component {
   render() {
     return (
       <div>
 
-        <nav className="navbar navbar-default navbar-fixed-top">
-          <div className="container">
-            <div className="navbar-header">
-              <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-                <span className="sr-only">Toggle navigation</span>
-                <span className="icon-bar"></span>
-                <span className="icon-bar"></span>
-                <span className="icon-bar"></span>
-              </button>
-              <a className="navbar-brand" href="#">Project name</a>
-            </div>
-            <div id="navbar" className="navbar-collapse collapse">
-              <ul className="nav navbar-nav">
-                <li className="active"><a href="#">Home</a></li>
-                <li><a href="#about">About</a></li>
-                <li><a href="#contact">Contact</a></li>
-                <li className="dropdown">
-                  <a href="#" className="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span className="caret"></span></a>
-                  <ul className="dropdown-menu">
-                    <li><a href="#">Action</a></li>
-                    <li><a href="#">Another action</a></li>
-                    <li><a href="#">Something else here</a></li>
-                    <li role="separator" className="divider"></li>
-                    <li className="dropdown-header">Nav header</li>
-                    <li><a href="#">Separated link</a></li>
-                    <li><a href="#">One more separated link</a></li>
-                  </ul>
-                </li>
-              </ul>
-              <ul className="nav navbar-nav navbar-right">
-                <li><a href="../navbar/">Default</a></li>
-                <li><a href="../navbar-static-top/">Static top</a></li>
-                <li className="active"><a href="./">Fixed top <span className="sr-only">(current)</span></a></li>
-              </ul>
-            </div>
-          </div>
-        </nav>
+        <Navbar fixedTop>
+          <Navbar.Header>
+            <Navbar.Brand><a href="#">Been There, Done That</a></Navbar.Brand>
+            <Navbar.Toggle />
+          </Navbar.Header>
+          <Navbar.Collapse>
+            <Navbar.Form pullRight>
+              <ButtonToolbar>
+
+              {/* TODO: change style to outline button with bootstrap v4 */}
+              <ButtonGroup><Button bsStyle="link">Log In</Button></ButtonGroup>
+
+              <ButtonGroup><Button bsStyle="success">Sign Up</Button></ButtonGroup>
+              </ButtonToolbar>
+            </Navbar.Form>
+            <Navbar.Form pullLeft>
+              <FormGroup>
+                <FormControl id="search-box" type="text" placeholder="Search..." />
+              </FormGroup>
+            </Navbar.Form>
+          </Navbar.Collapse>
+        </Navbar>
 
         <div className="container">
           <div className="jumbotron">
