14d73d768688b369f9b728784ddb0ace370a6493
Michael Fogel <mike@fogel.ca>
2017-05-08 17:07:53 -0300

    Seperate out tiles renderer

--- src/App.js
+++ src/App.js
@@ -2,23 +2,14 @@
 
 import React, { Component } from 'react';
 import NavBar from './renders/navbar.js'
+import Tiles from './renders/tiles.js'
 
 class App extends Component {
   render() {
     return (
       <div>
         <NavBar />
-        <div className="container">
-          <div className="jumbotron">
-            <h1>Navbar example</h1>
-            <p>This example is a quick exercise to illustrate how the default, static and fixed to top navbar work. It includes the responsive CSS and HTML, so it also adapts to your viewport and device.</p>
-            <p>To see the difference between static and fixed top navbars, just scroll.</p>
-            <p>
-              <a className="btn btn-lg btn-primary" href="../../components/#navbar" role="button">View navbar docs &raquo;</a>
-            </p>
-          </div>
-        </div>
-
+        <Tiles />
       </div>
     );
   }
---
+++ src/renders/tiles.js
@@ -0,0 +1,39 @@
+// @flow
+
+import React from 'react';
+import { Grid, Col, Row, Thumbnail, Button } from 'react-bootstrap'
+
+const Tile = () => (
+  <Col xs={12} sm={6} md={4} lg={3}>
+    <Thumbnail src="https://react-bootstrap.github.io/assets/thumbnaildiv.png" alt="242x200">
+      <h3>Thumbnail label</h3>
+      <p>Description</p>
+      <p>
+        <Button bsStyle="primary">Button</Button>
+	{' '}
+        <Button bsStyle="default">Button</Button>
+      </p>
+    </Thumbnail>
+  </Col>
+)
+
+const Tiles = () => (
+  <Grid>
+    <Row>
+      <Tile />
+      <Tile />
+      <Tile />
+      <Tile />
+      <Tile />
+      <Tile />
+      <Tile />
+      <Tile />
+      <Tile />
+      <Tile />
+      <Tile />
+      <Tile />
+    </Row>
+  </Grid>
+)
+
+export default Tiles
