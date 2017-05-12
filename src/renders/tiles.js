5f4dfcc3d0fd43df0b38cad4030fe88eabd3be49
Michael Fogel <mike@fogel.ca>
2017-05-11 22:07:36 -0300

    Get most tile metadata positioning set

--- src/renders/tiles.js
+++ src/renders/tiles.js
@@ -5,14 +5,21 @@ import { Grid, Col, Row, Image, ButtonToolbar, Button } from 'react-bootstrap'
 import './tiles.css';
 
 const Tile = () => (
-  <Col className="tile" xs={12} sm={6} md={4} lg={3}>
-    <ButtonToolbar className="tile-actions">
-      <Button bsStyle="success"><span className="glyphicon glyphicon-ok"></span></Button>
-      <Button bsStyle="info"><span className="glyphicon glyphicon-refresh"></span></Button>
-      <Button bsStyle="warning"><span className="glyphicon glyphicon-star"></span></Button>
-    </ButtonToolbar>
-    <Image src="http://placehold.it/480x360" alt="480x360" rounded responsive/>
-    <h3 className="tile-description">Hike the Inca trail</h3>
+  <Col xs={12} sm={6} md={4} lg={3}>
+    <div className="tile">
+      <ButtonToolbar className="tile-actions">
+        <Button bsStyle="success"><span className="glyphicon glyphicon-ok"></span></Button>
+        <Button bsStyle="info"><span className="glyphicon glyphicon-refresh"></span></Button>
+        <Button bsStyle="warning"><span className="glyphicon glyphicon-star"></span></Button>
+      </ButtonToolbar>
+      <Image src="http://placehold.it/480x360" alt="480x360" rounded responsive/>
+      <p className="tile-bottom-bar">
+        <h3 className="tile-title">Hike the Inca trail</h3>
+        <ul className="tile-tag-list">
+          <li><Button className="tile-tag" bsSize="xs" bsStyle="info"><span className="glyphicon glyphicon-tag"></span>travel</Button></li>
+        </ul>
+      </p>
+    </div>
   </Col>
 )
 
