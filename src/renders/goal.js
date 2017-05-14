6664ad2b185fc6002bae6c79b6ad1f6fc93a2830
Michael Fogel <mike@fogel.ca>
2017-05-14 16:28:58 -0300

    Styled up basic simple goal page

--- src/renders/goal.js
+++ src/renders/goal.js
@@ -1,8 +1,24 @@
 import React from 'react'
+import { Button } from 'react-bootstrap'
+import './goal.css';
+
+const sampleGoal = {
+  id: Math.floor(Math.random()*10000),
+  slug: 'travel-kansas',
+  title: 'Travel Kansas',
+  tags: ['travel'],
+  imageUrl: '/images/states.svg',
+}
 
 const Goal = ({match}) => (
-  <div>
-    <h3>{match.params.id}</h3>
+  <div className="goal" style={{backgroundImage: 'url(' + sampleGoal.imageUrl + ')'}}>
+    <div className="goal-topbar">
+      <div className="goal-title">
+        <h3>{sampleGoal.title}:</h3>
+        <h4><em><span className="glyphicon glyphicon-remove"></span>Incomplete</em></h4>
+      </div>
+      <p className="goal-actions"><Button bsStyle="success"><span className="glyphicon glyphicon-ok"></span></Button></p>
+    </div>
   </div>
 )
 
